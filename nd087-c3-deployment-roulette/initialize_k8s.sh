487cbed4c4310c2a7da6caf35eaf00ec78cd1031
vietlt <95838672+vietlt@users.noreply.github.com>
2023-10-21 19:20:05 +0700

    Delete nd087-c3-deployment-roulette directory

--- nd087-c3-deployment-roulette/initialize_k8s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-kubectl apply -f starter/apps/hello-world
-kubectl apply -f starter/apps/canary/index_v1_html.yml
-kubectl apply -f starter/apps/canary/canary-v1.yml
-kubectl apply -f starter/apps/blue-green
